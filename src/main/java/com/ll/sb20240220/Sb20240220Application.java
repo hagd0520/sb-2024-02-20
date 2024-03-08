2fe3d5d8ed76afa178204ef8870ea7270c837bf0
hagd <hagd0520@gmail.com>
2024-03-08 11:02:53 +0900

    init

---
+++ src/main/java/com/ll/sb20240220/Sb20240220Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20240220Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20240220Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb20240220/Sb20240220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20240220ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
